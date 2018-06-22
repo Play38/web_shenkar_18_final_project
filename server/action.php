c1eac72592497b8160c357a9315c0d85a19fc40b
guttman10 <37619448+guttman10@users.noreply.github.com>
2018-06-23 00:27:02 +0300

    now mirgates with sql server

--- server/action.php
+++ server/action.php
@@ -3,20 +3,36 @@
 	$dbuser = "studDB18A" ;
 	$dbpass = "stud18aDB1!" ;
 	$dbname = "studDB18A" ;
-	$conn = mysqli_connect ($dbhost, $dbuser, $dbpass, $dbname);
+	$link = mysqli_connect ($dbhost, $dbuser, $dbpass, $dbname);
 	
 	if(mysqli_connect_errno ()) {
 		die("DB connection failed: " . mysqli_connect_error () . " (" .
 		mysqli_connect_errno () . ")"
 );
 }
-	$sql = "INSERT INTO sender_list_204
-	VALUES ('lets', 'try', 'again','2','0','4:20:0','10')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+function generateRandomName() {
+	$fnames = array("Name","Asher","Dana","Yakov","Tal","Guts","Gundam","Orel");
+	$lnames = array("The king","Katz","Guttman","Cohen","Abir","Lastname2k","Lonet");
+	$randomName = '';
+	$space = ' ';
+		$randomName .= $fnames[mt_rand(0, count($fnames) - 1)];
+		$randomName .= $space;
+		$randomName .= $lnames[mt_rand(0, count($lnames) - 1)];
+    return $randomName;
 }
-    mysqli_close($conn);
+$name = generateRandomName();
+$pickup = mysqli_real_escape_string($link, $_REQUEST['pickup']);
+$dest = mysqli_real_escape_string($link, $_REQUEST['dest']);
+$weight = mysqli_real_escape_string($link, $_REQUEST['weight']);
+$breakable = mysqli_real_escape_string($link, $_REQUEST['breakable']);
+$amount = mysqli_real_escape_string($link, $_REQUEST['amount']);
+$time= mysqli_real_escape_string($link, $_REQUEST['time']);
+$sql = "INSERT INTO sender_list_204 VALUES ('$name', '$pickup', '$dest','$weight','$breakable','$amount','$time')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+mysqli_close($link);
 ?>
