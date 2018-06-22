cb23edbfb3d6241ea2430a97023924ace2917d07
guttman10 <37619448+guttman10@users.noreply.github.com>
2018-06-22 21:42:18 +0300

    updated server things

--- server/action.php
+++ server/action.php
@@ -1,34 +1,22 @@
 ﻿<?php
-    include('db.php');
+   	$dbhost = "182.50.133.51" ;
+	$dbuser = "studDB18A" ;
+	$dbpass = "stud18aDB1!" ;
+	$dbname = "studDB18A" ;
+	$conn = mysqli_connect ($dbhost, $dbuser, $dbpass, $dbname);
+	
+	if(mysqli_connect_errno ()) {
+		die("DB connection failed: " . mysqli_connect_error () . " (" .
+		mysqli_connect_errno () . ")"
+);
+}
+	$sql = "INSERT INTO sender_list_204
+	VALUES ('lets', 'try', 'again','2','0','4:20:0','10')";
 
-        //escape variables for security
-        $pickupe = mysqli_real_escape_string($connection, $_POST['pickup']);
-        $deste = mysqli_real_escape_string($connection, $_POST['dest']);
-        $weighte = mysqli_real_escape_string($connection, $_POST['weight']);
-        $breakablee = mysqli_real_escape_string($connection, $_POST['breakable']);
-        $amounte = mysqli_real_escape_string($connection, $_POST['amount']);
-        $timee = mysqli_real_escape_string($connection, $_POST['time']);
-    
-        //SET: insert new data to DB
-        $query2 = "INSERT into sender_list_204(pickup, dest, weight, breakable, time, amount) values ('$pickupe', '$deste', '$weighte', '$breakablee', '$timee', '$amounte')";
-        $result = mysqli_query($connection, $query2);
-        /*
-        //GET: get data again
-        $query2 = "SELECT * FROM tbl_test order by title desc";
-        $result = mysqli_query($connection, $query2);
-        */
-    /*
-    //GET: get data again
-    echo "<ul>";
-    while($row = mysqli_fetch_assoc($result)){ //result are in an associative array. keys are cols names 
-        echo "<li><h2>" . $row["title"] . "</h2><h6>" . $row["txt"] . "</h6></li>";
-    }
-    echo "</ul>";
-    */
-    //release returned data
-    
-    //mysqli_free_result($result);
-
-    //close DB connection
-    mysqli_close($connection);
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+    mysqli_close($conn);
 ?>
