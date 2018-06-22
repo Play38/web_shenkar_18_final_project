93792720f77e687180888d9b1faa63eeb4d3a15c
Play38 <ofir.cohen@stu.shenkar.ac.il>
2018-06-22 20:03:40 +0300

    added varaiables

--- server/action.php
+++ server/action.php
@@ -2,7 +2,7 @@
     include('db.php');
     
     //get data from DB to display
-    $query1 = "SELECT * FROM sender_list_204 order by title desc";    
+    $query1 = "SELECT * FROM sender_list_204 order by name pickup dest weight breakable time amount";    
     $result = mysqli_query($connection, $query1);
 
     if(!$result) {
@@ -21,14 +21,14 @@
         $query2 = "SELECT * FROM tbl_test order by title desc";
         $result = mysqli_query($connection, $query2);
     }
-    
+    /*
     //GET: get data again
     echo "<ul>";
     while($row = mysqli_fetch_assoc($result)){ //result are in an associative array. keys are cols names 
         echo "<li><h2>" . $row["title"] . "</h2><h6>" . $row["txt"] . "</h6></li>";
     }
     echo "</ul>";
-    
+    */
     //release returned data
     mysqli_free_result($result);
 
