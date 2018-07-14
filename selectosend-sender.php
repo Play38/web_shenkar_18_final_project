83d93ed16aeae7a0f5b6abfcf7e4283f05ba167f
Play38 <ofir.cohen@stu.shenkar.ac.il>
2018-07-14 17:10:21 +0300

    detects when there are 0 raws

--- selectosend-sender.php
+++ selectosend-sender.php
@@ -4,7 +4,7 @@
 	$dbpass = "stud18aDB1!" ;
     $dbname = "studDB18A" ;
 	$link = mysqli_connect ($dbhost, $dbuser, $dbpass, $dbname);
-	
+	$zeroRaws= 0;
 	if(mysqli_connect_errno ()) {
 		die("DB connection failed: " . mysqli_connect_error () . " (" .
 		mysqli_connect_errno () . ")"
@@ -142,8 +142,11 @@ $query1 = "SELECT * FROM `sender_list_204` WHERE $start <= time AND time <= $end
                                                 </section>
                                             </div>
                                         </div>" ;
-                                       
+                                        $zeroRaws++;
                                     }
+
+                                    if($zeroRaws == 0)
+                                        echo"test";
                                         mysqli_close($link);
             
         ?>
